a1b5de7ab064c0ac8ab2993a5469fbfbc6182f03
Hykudoru <iamalexfish@gmail.com>
2023-05-14 18:14:08 -0500

    Camera

---
+++ graphics.js
@@ -0,0 +1,212 @@
+    //-----------GRAPHICS---------------
+    var forward = { x: 0, y: 0, z: -1 };
+    var back = { x: 0, y: 0, z: 1 };
+    var right = { x: 1, y: 0, z: 0 };
+    var left = { x: -1, y: 0, z: 0 };
+    var up = { x: 0, y: -1, z: 0 };
+    var down = { x: 0, y: 1, z: 0 };
+    
+    var orthographicProjectionMatrix = [
+        [1, 0, 0],
+        [0, 1, 0],
+        [0, 0, 0]
+    ];
+
+    var fov = 15*Math.PI/180.0;
+    let perspectiveProjectionMatrix = [
+        [1.0/Math.tan(fov/2), 0, 0, 0],
+        [0, 1.0/Math.tan(fov/2), 0, 0],
+        [0, 0, 1, 0],
+        [0, 0, -1, 0]
+    ];
+
+    class Transform
+    {
+        scale = new Vec3(1, 1, 1);
+        position = new Vec3(0, 0, 0);
+        rotation = [
+            [1, 0, 0],
+            [0, 1, 0],
+            [0, 0, 1]
+        ];
+        get forward() {return MatrixVectorMultiply(this.rotation, forward);}
+        get back() {return MatrixVectorMultiply(this.rotation, back);}
+        get right() {return MatrixVectorMultiply(this.rotation, right);}
+        get left() {return MatrixVectorMultiply(this.rotation, left);}
+        get up() {return MatrixVectorMultiply(this.rotation, up);}
+        get down() {return MatrixVectorMultiply(this.rotation, down);}
+        get localPosition() {return Matrix4x4VectorMult(this.translationMatrix4x4Inverse, new Vec4(this.position.x, this.position.y, this.position.z, 1));}
+        // constructor(position = new Vec3(0, 0, 0), rotationEuler = new Vec3(0, 0, 0), scale = new Vec3(1, 1, 1))
+        // {
+        //     this.scale = scale;
+        //     this.position = position;
+        //     this.rotation = YPR(rotationEuler.x, rotationEuler.y, rotationEuler.z);
+        // }
+        constructor(scale = 1) {
+            this.scale.x = scale;
+            this.scale.y = scale;
+            this.scale.z = scale;
+        }
+
+        get scalingMatrix4x4() 
+        {
+            return [
+                [this.scale.x, 0, 0, 0],
+                [0, this.scale.y, 0, 0],
+                [0, 0, this.scale.z, 0],
+                [0, 0, 0, 1]
+            ];
+        }
+
+        get rotationMatrix4x4() {
+                return [
+                    [this.rotation[0][0], this.rotation[0][1], this.rotation[0][2], 0],
+                    [this.rotation[1][0], this.rotation[1][1], this.rotation[1][2], 0],
+                    [this.rotation[2][0], this.rotation[2][1], this.rotation[2][2], 0],
+                    [       0,                      0,                  0,          1]
+                ];
+        }        
+
+        get translationMatrix4x4() {
+            return [
+                [1, 0, 0, this.position.x],
+                [0, 1, 0, this.position.y],
+                [0, 0, 1, this.position.z],
+                [0, 0, 0, 1]
+            ];
+        }
+
+        get translationMatrix4x4Inverse() {
+            return [
+                [1, 0, 0, -this.position.x],
+                [0, 1, 0, -this.position.y],
+                [0, 0, 1, -this.position.z],
+                [0, 0, 0, 1]
+            ];
+        }
+
+        get TRS() {
+            return MatrixMult4x4(MatrixMult4x4(this.translationMatrix4x4, this.rotationMatrix4x4), this.scalingMatrix4x4);
+            //return MatrixMult4x4(this.translationMatrix4x4, MatrixMult4x4(this.rotationMatrix4x4, this.scalingMatrix4x4));
+        }
+
+        get TR() {
+            return MatrixMult4x4(this.translationMatrix4x4, this.rotationMatrix4x4);
+
+            // return [
+            //         [this.rotation[0][0], this.rotation[0][1], this.rotation[0][2],  this.position.x],
+            //         [this.rotation[1][0], this.rotation[1][1], this.rotation[1][2],  this.position.y],
+            //         [this.rotation[2][0], this.rotation[2][1], this.rotation[2][2],  this.position.z],
+            //         [       0,                      0,                  0,          1]
+            //     ];
+        }
+
+        get TRInverse() {
+            return MatrixMult4x4(Transpose4x4(this.rotationMatrix4x4), this.translationMatrix4x4Inverse);
+        }
+    }
+
+    class Camera extends Transform
+    {
+
+    }
+
+    let camera = new Transform();
+
+    class CubeMesh extends Transform 
+    {
+        // Local Space (Object Space)
+        vertices = [
+            { x: -1, y: 1, z: 1 },
+            { x: -1, y: -1, z: 1 },
+            { x: 1, y: -1, z: 1 },
+            { x: 1, y: 1, z: 1 },
+            //forward
+            { x: -1, y: 1, z: -1 },
+            { x: -1, y: -1, z: -1 },
+            { x: 1, y: -1, z: -1 },
+            { x: 1, y: 1, z: -1 }
+        ];
+
+        projVertices = [];
+
+        get triangles() {
+            let triangles = [
+                //south
+                [this.projVertices[0], this.projVertices[1], this.projVertices[2]],
+                [this.projVertices[0], this.projVertices[2], this.projVertices[3]],
+                //north
+                [this.projVertices[7], this.projVertices[6], this.projVertices[5]],
+                [this.projVertices[7], this.projVertices[5], this.projVertices[4]],
+                //right
+                [this.projVertices[3], this.projVertices[2], this.projVertices[6]],
+                [this.projVertices[3], this.projVertices[6], this.projVertices[7]],
+                //left
+                [this.projVertices[4], this.projVertices[5], this.projVertices[1]],
+                [this.projVertices[4], this.projVertices[1], this.projVertices[0]],
+                //top
+                [this.projVertices[1], this.projVertices[5], this.projVertices[6]],
+                [this.projVertices[1], this.projVertices[6], this.projVertices[2]],
+                //bottom
+                [this.projVertices[3], this.projVertices[7], this.projVertices[4]],
+                [this.projVertices[3], this.projVertices[4], this.projVertices[0]],
+            ];
+
+            return triangles;
+        }
+        
+        transformVertices() {
+            //Transform
+            for (let i = 0; i < this.vertices.length; i++) {
+                
+                // local space (Object space) coords (x, y, z) each point range [-1, 1]
+                let vert = this.vertices[i];
+                
+                // Homogeneous coords (x, y, z, w=1)
+                vert = new Vec4(this.vertices[i].x, this.vertices[i].y, this.vertices[i].z, 1);
+
+                // ======== World space ========
+                // Transform local coords to world-space coords.
+                
+                let modelToWorldMatrix = this.TRS;
+                let worldPoint = Matrix4x4VectorMult(modelToWorldMatrix, vert);
+                
+                // ====== View space ========
+                // (Camera space, Eye space). Transform world coordinates to view coordinates.
+                let worldToViewMatrix = camera.TRInverse;
+                let cameraSpacePoint = Matrix4x4VectorMult(worldToViewMatrix, worldPoint);
+                
+                // ======== Screen space ==========
+                // Project to screen space (image space) 
+                let perspPoint = Matrix4x4VectorMult(perspectiveProjectionMatrix, cameraSpacePoint);
+                
+                perspPoint = VectorScale(perspPoint, 100);//this.scale.x);
+               
+                this.projVertices[i] = perspPoint;
+            }
+        }
+
+        drawTriangles() {
+            // Draw Triangles
+            for (let t = 0; t < this.triangles.length; t++) {
+                strokeWeight(2);
+                let triPoints = this.triangles[t];
+                let p1 = triPoints[0];
+                let p2 = triPoints[1];
+                let p3 = triPoints[2];
+            
+                if (this.position.z < 3) {
+                    line(p1.x, p1.y, 0, p2.x, p2.y, 0);
+                    line(p2.x, p2.y, 0, p3.x, p3.y, 0);
+                    line(p3.x, p3.y, 0, p1.x, p1.y, 0);
+                }
+            }
+        }
+
+        DrawMesh() {
+            this.transformVertices();
+            this.drawTriangles();
+        }
+    }
+
+    var cube = new CubeMesh(20);
---
+++ matrix.js
@@ -0,0 +1,218 @@
+// Returns a Rotation Matrix about the X axis (in radians)
+    // | 1,   0,      0     |
+    // | 0, Cos(T), -Sin(T) |
+    // | 0, Sin(T),  Cos(T) |
+    function RotX(theta) {
+        let Cos = Math.cos(theta);
+        let Sin = Math.sin(theta);
+
+        let matrix = [
+            [1, 0, 0],
+            [0, Cos, -Sin],
+            [0, Sin, Cos]
+        ];
+
+        return matrix;
+    }
+
+    // Returns a Rotation Matrix about the Y axis (in radians)
+    // | Cos(T),  0,   Sin(T)  |
+    // |   0,     1,     0     |
+    // |-Sin(T),  0,   Cos(T)  |
+    function RotY(theta) {
+        let Cos = Math.cos(theta);
+        let Sin = Math.sin(theta);
+
+        let matrix = [
+            [Cos, 0, Sin],
+            [0, 1, 0],
+            [-Sin, 0, Cos]
+        ];
+
+        return matrix;
+    }
+
+    // Returns a Rotation Matrix about the Z axis (in radians)
+    // | Cos(T), -Sin(T), 0 |
+    // | Sin(T),  Cos(T), 0 |
+    // |   0,      0,     1 |
+    function RotZ(theta) {
+        let Cos = Math.cos(theta);
+        let Sin = Math.sin(theta);
+
+        let matrix = [
+            [Cos, -Sin, 0],
+            [Sin, Cos, 0],
+            [0, 0, 1]
+        ];
+
+        return matrix;
+    }
+
+    // Yaw-Pitch-Roll z-y'-x''(intrinsic rotation) or x-y-z (extrinsic rotation)
+    function YPR(roll, pitch, yaw) {
+        let rotationMatrix = MatrixMultiply(MatrixMultiply(RotZ(yaw), RotY(pitch)), RotX(roll));//MatrixMultiply(rotation.matrix, RotZ(PI/2.0).matrix);
+        return rotationMatrix;
+    }
+
+class Vec3 
+{
+    v = [0, 0, 0];
+    get x() { return this.v[0];} set x(val) {this.v[0] = val;}
+    get y() { return this.v[1];} set y(val) {this.v[1] = val;}
+    get z() { return this.v[2];} set z(val) {this.v[2] = val;}
+
+    constructor(x = 0, y = 0, z = 0) {
+        this.v[0] = x;
+        this.v[1] = y;
+        this.v[2] = z;
+    }
+
+    ToString() {
+        let str = "";
+        this.v.forEach(element => {
+            str += " "+element;
+        });
+        str += "\n\r";
+
+        return str;
+    }
+}
+
+class Vec4 extends Vec3 
+{
+    get w() { return this.v[3]} set w(val) {this.v[3] = val;}
+
+    constructor(x = 0, y = 0, z = 0, w = 1) {
+        super(x, y, z);
+        this.v[3] = w;
+    }
+}
+
+class Matrix3x3 
+{
+    matrix = [
+        [1, 0, 0],
+        [0, 1, 0],
+        [0, 0, 1]
+    ];
+    constructor(mtx3x3)
+    {
+        let matrix = [
+            [1, 0, 0],
+            [0, 1, 0],
+            [0, 0, 1]
+        ];
+
+        this.matrix = mtx3x3 || matrix;
+    }
+}
+
+class Matrix4x4 
+{    
+    constructor(mtx4x4)
+    {
+        let matrix = [
+            [1, 0, 0, 0],
+            [0, 1, 0, 0],
+            [0, 0, 1, 0],
+            [0, 0, 0, 1]
+        ];
+
+        this.matrix = mtx4x4 || matrix;
+    }
+}
+
+    function VectorScale(vector, scaler) {
+        let vec = new Vec3();
+        vec.x = vector.x * scaler;
+        vec.y = vector.y * scaler;
+        vec.z = vector.z * scaler;
+
+        return vec;
+    }
+
+    function VectorSum(vec1, vec2) {
+        let vectorSum = new Vec3((vec1.x + vec2.x), (vec1.y + vec2.y), (vec1.z + vec2.z));
+        return vectorSum;
+    }
+
+    function DotProduct(vecA, vecB) {
+        return (vecA.x * vecB.x) + (vecA.y * vecB.y) + (vecA.z * vecB.z);
+    }
+
+    function CrossProduct(vecA, vecB) {
+        
+    }
+
+    function MatrixMultiply(matrix, columnMatrix) {
+        let mtx = new Matrix3x3();
+        for (var r = 0; r < 3; r++) {
+            let rowVec = new Vec3(matrix[r][0], matrix[r][1], matrix[r][2]);
+            for (var c = 0; c < 3; c++) {
+                let columnVec = new Vec3(columnMatrix[0][c], columnMatrix[1][c], columnMatrix[2][c]);
+                mtx.matrix[r][c] = DotProduct(rowVec, columnVec);
+            }
+        }
+
+        return mtx.matrix;
+    }
+
+    function MatrixVectorMultiply(matrix, columnVector) {
+        let result = [0, 0, 0];
+        for (var r = 0; r < 3; r++) {
+            let rowVec = new Vec3(matrix[r][0], matrix[r][1], matrix[r][2]);
+            result[r] = DotProduct(rowVec, columnVector);
+        }
+
+        return new Vec3(result[0], result[1], result[2]);
+    }
+
+    //--------------- 4x4 ---------------------
+
+    function Transpose4x4(matrix) {
+        let mtx = new Matrix4x4();
+
+        for (var r = 0; r < 4; r++) {
+            mtx.matrix[0][r] = matrix[r][0];
+            mtx.matrix[1][r] = matrix[r][1];
+            mtx.matrix[2][r] = matrix[r][2];
+            mtx.matrix[3][r] = matrix[r][3];
+        }
+
+        return mtx.matrix;
+    }
+
+    function Dot4(vecA, vecB) {
+        return (vecA.x * vecB.x) + (vecA.y * vecB.y) + (vecA.z * vecB.z) + (vecA.w * vecB.w);
+    }
+
+    function MatrixMult4x4(matrix, columnMatrix) {
+        let mtx = new Matrix4x4();
+        for (var r = 0; r < 4; r++) {
+            let rowVec = new Vec4(matrix[r][0], matrix[r][1], matrix[r][2], matrix[r][3]);
+            for (var c = 0; c < 4; c++) {
+                let columnVec = new Vec4(columnMatrix[0][c], columnMatrix[1][c], columnMatrix[2][c], columnMatrix[3][c]);
+                mtx.matrix[r][c] = Dot4(rowVec, columnVec);
+            }
+        }
+
+        return mtx.matrix;
+    }
+
+    function Matrix4x4VectorMult(matrix, vec4) {
+        let result = new Vec4();
+        for (var r = 0; r < 4; r++) {
+            let rowVec = new Vec4(matrix[r][0], matrix[r][1], matrix[r][2], matrix[r][3]);
+            result.v[r] = Dot4(rowVec, vec4);
+        }
+
+        if (result.w != 0.0) {
+            result.x /= result.w;
+            result.y /= result.w;
+            result.z /= result.w;
+            // result.w /= result.w;
+        }
+
+        return result;
+    } 
